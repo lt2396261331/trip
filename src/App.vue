821d3151226315d5497f8520d04696236e775845
lt2396261331 <yan1283298696@163.com>
2022-12-14 17:14:34 +0800

    init: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <h2>App</h2>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/favor/favor.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="faovr">
+    <h2>faovr</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
