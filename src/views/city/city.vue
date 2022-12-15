a00660849d069726b1d4f9cf936cfec10b4147bc
suyanCode <yan1283299899@163.com>
2022-12-15 22:01:20 +0800

    feat: 根据navbar筛选数据

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -11,17 +11,22 @@
     <!-- 2.tab的切换 -->
     <van-tabs v-model:active="activeName" color="#ff9854">
       <template v-for="(value, key, index) in allCities">
-        <van-tab :title="value.title"></van-tab>
+        <van-tab :title="value.title" :name="key"></van-tab>
       </template>
     </van-tabs>
+
+    <!-- 城市列表 -->
+    <city-group />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
 import useCityStore from '@/stores/modules/city'
+
+import CityGroup from './cpns/city-group.vue'
 
 const router = useRouter()
 
@@ -39,6 +44,9 @@ const cityStore = useCityStore()
 cityStore.fetchAllCitiesData()
 // storeToRefs函数进行结构不会失去响应式
 const { allCities } = storeToRefs(cityStore)
+// 根据选择的标签获取对应的数据
+const cityGroup = computed(() => allCities.value[activeName.value])
+
 
 </script>
 
---
+++ src/views/city/cpns/city-group.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="city-group">
+    <van-index-bar>
+      <van-index-anchor index="A" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+
+      <van-index-anchor index="B" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+    </van-index-bar>
+  </div>
+</template>
+
+<script setup>
+
+defineProps({
+  groupData: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
